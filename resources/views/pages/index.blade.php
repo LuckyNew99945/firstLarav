9fc571659cf4220305370a2329e7135d044a507a
LuckyNew99945 <luckynew99945@gmail.com>
2020-08-08 21:40:45 +0700

    initialize firstLarav,basic routing and controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'firstLarav')}}</title>
+
+      
+    </head>
+    <body>
+       <h1>Welcome to firstLarav</h1>
+       <p>This is a my first laravel and php </p>
+    </body>
+</html>
