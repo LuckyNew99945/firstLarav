90ba902b3a53d16f5b86be5dc7a24accb638d6a1
LuckyNew99945 <luckynew99945@gmail.com>
2020-08-08 21:52:12 +0700

    controllers in Laravel

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'firstLarav')}}</title>
+
+      
+    </head>
+    <body>
+       <h1>Services</h1>
+       <p>This is services page </p>
+    </body>
+</html>
