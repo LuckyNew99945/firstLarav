a8b7b577290d7c84d74733e60a2466c3b9d4f211
LuckyNew99945 <luckynew99945@gmail.com>
2020-08-09 23:02:24 +0700

    update and delete record from MySQL in Laravel

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -78,7 +78,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -90,7 +91,19 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+       $post = Post::find($id);
+       $post->title = $request->input('title');
+       
+       $post->body = $request->input('body');
+
+       $post->save();
+
+       return redirect('/posts')->with('success','Post edit successfully');
     }
 
     /**
@@ -101,6 +114,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post delete successfully');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
 @section('content')
   <a href="/posts" class="btn btn-default">Go Back</a>
@@ -6,5 +6,14 @@
   <p>{{$post->body}}</p>
   <hr/>
   <small>Written on {{$post->created_at}}</small>
+  <hr/>
+  <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+  {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+
+  {{Form::hidden('_method', 'DELETE')}}
+
+  {{Form::submit('delete', ['class' => 'btn btn-danger'])}}
+
+  {!!Form::close()!!}
 
   @endsection
